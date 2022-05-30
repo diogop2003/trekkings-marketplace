b07084b05c463d1b5f23d62e0f0d6cca48e0ebb4
Diogo Prado <diogoprado1511@gmail.com>
2022-05-30 15:52:00 -0300

    Order Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :trekkings, only: %i[index create new]
+  resources :trekkings, only: %i[index create new] do
+    resources :orders
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
