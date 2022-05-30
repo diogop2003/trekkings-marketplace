91805911135f3382f33708edc838afc752c1b52f
Diogo Prado <diogoprado1511@gmail.com>
2022-05-30 15:41:04 -0300

    Fix new button

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_175546) do
+ActiveRecord::Schema.define(version: 2022_05_30_182941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.bigint "trekking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["trekking_id"], name: "index_orders_on_trekking_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "trekkings", force: :cascade do |t|
     t.string "name"
@@ -38,4 +48,6 @@ ActiveRecord::Schema.define(version: 2022_05_30_175546) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "trekkings"
+  add_foreign_key "orders", "users"
 end
