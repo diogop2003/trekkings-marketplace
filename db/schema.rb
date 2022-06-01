9da27a742eda41cd7afc0fa5862a2e3f86836dfd
Diogo Prado <diogoprado1511@gmail.com>
2022-06-01 11:58:01 -0300

    Add dotenv - Protect my secret data in .env file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_194113) do
+ActiveRecord::Schema.define(version: 2022_06_01_143118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_05_30_194113) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "url_image"
+    t.string "description"
     t.index ["user_id"], name: "index_trekkings_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,9 @@ s = User.create(name: 'Sorriso', email: 'sr@gmail.com', password: '123123')
 
 puts 'Creating Trekking...'
 
-t = Trekking.create(name: 'Backpack', price: 25.50, category: 'Backpack', user: d, url_image: 'https://ae01.alicdn.com/kf/H5b07cc8ac63d456b86eb0e35182993486/Mochila-de-60l-para-trilha-bolsa-prova-d-gua-para-acampamento-viagem-ao-ar-livre-mochila.jpg_Q90.jpg_.webp')
-y = Trekking.create(name: 'Shoes', price: 40.90, category: 'Shoes', user: h, url_image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQG5Kgu-z32r41Kd6W5TvO5t8QNDHw92tMwo-wjaEHE4aj9Hvtw54h9pxZbkRBzEDwOIJiyXD-Lvuksl0fenKBBV707wdZn7zuMmSdpx2TYcUhCoe8JHVlidw&usqp=CAE')
-u = Trekking.create(name: 'Coat', price: 30.90, category: 'Coat', user: g, url_image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSpq-XG7uDD-qAGBCFMzmO20pH3gjnEPLX5TRpAmf3B7Bw96H7Jud_luzpazf1wXZZ9r-vlf1pZOzELpxz6bPfQpW9-DBxjvz7xHoMNrUtrS_5J1fhOxshcBg&usqp=CAE')
+t = Trekking.create(name: 'Big Backpack', price: 25.50, category: 'Backpack', description: "The best Backpack in the world", user: d, url_image: 'https://ae01.alicdn.com/kf/H5b07cc8ac63d456b86eb0e35182993486/Mochila-de-60l-para-trilha-bolsa-prova-d-gua-para-acampamento-viagem-ao-ar-livre-mochila.jpg_Q90.jpg_.webp')
+y = Trekking.create(name: 'Great Shoes', price: 40.90, category: 'Shoes', description: "The best Shoes in the world", user: h, url_image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQG5Kgu-z32r41Kd6W5TvO5t8QNDHw92tMwo-wjaEHE4aj9Hvtw54h9pxZbkRBzEDwOIJiyXD-Lvuksl0fenKBBV707wdZn7zuMmSdpx2TYcUhCoe8JHVlidw&usqp=CAE')
+u = Trekking.create(name: 'Warm Coat', price: 30.90, category: 'Coat', description: "The best Coat in the world", user: g, url_image: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSpq-XG7uDD-qAGBCFMzmO20pH3gjnEPLX5TRpAmf3B7Bw96H7Jud_luzpazf1wXZZ9r-vlf1pZOzELpxz6bPfQpW9-DBxjvz7xHoMNrUtrS_5J1fhOxshcBg&usqp=CAE')
 
 puts 'Creating Order...'
 
