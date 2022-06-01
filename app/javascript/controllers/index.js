c38d80f057c066fc960214dbbb7ce2847111bc90
Heitor Bonanno <heitor.cb10@gmail.com>
2022-06-01 15:19:08 -0300

    navbar alteration

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,3 +7,13 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+}
