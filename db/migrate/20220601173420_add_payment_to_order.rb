58845380f3ba53d5396b04b21a7ccb805a30074f
Daniel Carvalho <daluvijama@gmail.com>
2022-06-02 12:32:23 -0300

    error

--- db/migrate/20220601173420_add_payment_to_order.rb
+++ db/migrate/20220601173420_add_payment_to_order.rb
@@ -1,5 +1,7 @@
 class AddPaymentToOrder < ActiveRecord::Migration[6.1]
   def change
-
+    add_column :orders, :number, :interger
+    add_column :orders, :complement, :text
+    add_column :orders, :cpf, :string
   end
 end
