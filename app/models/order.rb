109749bef3be770c034e9f22be95cd97bb223de8
Daniel Carvalho <daluvijama@gmail.com>
2022-06-02 13:25:17 -0300

    error

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :trekking
-  validates :address, presence: true
+  validates :address, :number, :complement, :name,  presence: true
+  validates :cpf, :format => { :with => /[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/}, presence: true
+  validates :cardnumber, :length => { :is => 16}
+  validates :code, :format => { :with => /[0-9]{3, 4}/}, presence: true
+  validates :validates, :format => { :with => /(0[1-9]|1[0-2])\/?([0-9]{2})/}, presence: true
 end
